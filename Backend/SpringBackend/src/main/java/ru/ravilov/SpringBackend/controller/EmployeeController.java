1e8dc83dd5733e82c9f132eab606fbd63cf76544
ScionRs <grazs116@gmail.com>
2021-07-22 17:40:41 +0300

    Added deleteEmployee method

--- Backend/SpringBackend/src/main/java/ru/ravilov/SpringBackend/controller/EmployeeController.java
+++ Backend/SpringBackend/src/main/java/ru/ravilov/SpringBackend/controller/EmployeeController.java
@@ -7,7 +7,9 @@ import ru.ravilov.SpringBackend.exception.ResourceNotFoundException;
 import ru.ravilov.SpringBackend.model.Employee;
 import ru.ravilov.SpringBackend.repository.EmployeeRepository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -44,5 +46,15 @@ public class EmployeeController {
 
         Employee updatedEmployee = employeeRepository.save(employee);
         return ResponseEntity.ok(updatedEmployee);
+    }
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id){
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id :" + id));
+
+        employeeRepository.delete(employee);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return ResponseEntity.ok(response);
     }
 }
