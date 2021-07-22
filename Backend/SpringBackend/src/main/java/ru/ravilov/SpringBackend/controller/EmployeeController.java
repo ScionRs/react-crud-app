1b5157cc8576df41acf5ddb1aad6da8cf463190f
ScionRs <grazs116@gmail.com>
2021-07-22 17:13:38 +0300

    Added updatedComponent

--- Backend/SpringBackend/src/main/java/ru/ravilov/SpringBackend/controller/EmployeeController.java
+++ Backend/SpringBackend/src/main/java/ru/ravilov/SpringBackend/controller/EmployeeController.java
@@ -34,4 +34,15 @@ public class EmployeeController {
                 .orElseThrow(()  -> new ResourceNotFoundException("Employee not exist with id:"));
         return ResponseEntity.ok(employee);
     }
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id,@RequestBody Employee employeeDetails){
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(()  -> new ResourceNotFoundException("Employee not exist with id:"));
+        employee.setFirstName(employeeDetails.getFirstName());
+        employee.setLastName(employeeDetails.getLastName());
+        employee.setEmailId(employeeDetails.getEmailId());
+
+        Employee updatedEmployee = employeeRepository.save(employee);
+        return ResponseEntity.ok(updatedEmployee);
+    }
 }
