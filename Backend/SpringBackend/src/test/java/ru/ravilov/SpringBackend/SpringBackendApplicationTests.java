435156352a5a5b4080f9a42b31fa7b20882f7dcc
ScionRs <grazs116@gmail.com>
2021-07-22 10:01:23 +0300

    Initial commit

---
+++ Backend/SpringBackend/src/test/java/ru/ravilov/SpringBackend/SpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ravilov.SpringBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
